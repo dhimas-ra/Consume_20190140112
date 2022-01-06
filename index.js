287185a18ca32b63942dcc13aece211c9504e30b
Dhimas <dhimas.ra48@gmail.com>
2022-01-06 12:47:02 +0700

    .

--- index.js
+++ index.js
@@ -26,14 +26,13 @@ $("#dataTable").ready(function () {
             const td3 = tr.insertCell();
             const td4 = tr.insertCell();
             const td5 = tr.insertCell();
-            const td6 = tr.insertCell();
+            
             console.log(response[i])
-            td1.innerHTML = response[i].idcust
-            td2.innerHTML = response[i].nama
-            td3.innerHTML = response[i].nohp
-            td4.innerHTML = response[i].jenis
-            td5.innerHTML = response[i].jumlah
-            td6.innerHTML = `<div class ="justify content-center">
+            td1.innerHTML = response[i].idc
+            td2.innerHTML = response[i].nama_mobil
+            td3.innerHTML = response[i].jenis_mobil
+            td4.innerHTML = response[i].plat_nomor
+            td5.innerHTML = `<div class ="justify content-center">
             <a class="btn ms-2" style="background-color: #858ab3; border:none;" href="updateData.html?idcust=${response[i].idcust}">Edit</a>
             <button type ="button" class="btn ms-2" style="background-color: #858ab3; border:none;" onclick="del(${response[i].idcust});">Delete</button>
             </div>`
